68c8ca809cb7d6747147a27df729c83e3016c49d
CodyTolene <codyatolene@gmail.com>
2019-05-01 22:17:14 -0500

    Updated Google Analytics implementation.

---
+++ development/scripts/analytics.js
@@ -0,0 +1,6 @@
+(function () {
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'UA-42346076-7');
+})();
--- development/scripts/main.js
+++
@@ -1,9 +0,0 @@
-(function (window, document, jQuery) {
-    var note = '[JavaScript] Initialized global file.';
-
-    function init() {
-        console.log(note);
-    }
-
-    init();
-})(window, document, jQuery);
