106268e6aa3bf812c63436bd03737dd88d3ef7f4
Cody Tolene <codyatolene@gmail.com>
2020-08-15 21:37:24 -0500

    Testing out addition of Google analytics

--- src/core/core.component.ts
+++ src/core/core.component.ts
@@ -1,14 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { Router, NavigationEnd } from '@angular/router';
+import { componentDestroyed } from '../shared/utilities';
+import { takeUntil } from 'rxjs/operators';
+
+declare let gtag;
 
 @Component({
   selector: 'app-core',
   template: `<router-outlet></router-outlet>`,
 })
-export class CoreComponent {
-  public constructor(private readonly serviceWorkerUpdate: SwUpdate) {
-    // Update the service worker on every construct
-    // This fixes and issue with GitHub pages 404 redirect into Angular
-    this.serviceWorkerUpdate.activateUpdate();
+export class CoreComponent implements OnDestroy {
+  public constructor(
+    private readonly serviceWorkerUpdate: SwUpdate,
+    private readonly router: Router,
+  ) {
+    if (environment.production) {
+      // Update the service worker on every construct
+      // This fixes and issue with GitHub pages 404 redirect into Angular
+      this.serviceWorkerUpdate.activateUpdate();
+
+      // Google analytics
+      this.router.events
+        .pipe(takeUntil(componentDestroyed(this)))
+        .subscribe((event) => {
+          if (event instanceof NavigationEnd) {
+            console.log(event.urlAfterRedirects);
+            gtag('js', new Date());
+            gtag('config', 'UA-42346076-7', {
+              page_path: event.urlAfterRedirects,
+            });
+          }
+        });
+    }
+  }
+
+  public ngOnDestroy(): void {
+    // Needed for `takeUntil(componentDestroyed(this))`
   }
 }
