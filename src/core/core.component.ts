8b9889f119bc46528ce73c187b85b276e3bbe7bf
Cody Tolene <codyatolene@gmail.com>
2020-08-15 19:18:41 -0500

    Force service worker refresh on site 404

--- src/core/core.component.ts
+++ src/core/core.component.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-core',
   template: `<router-outlet></router-outlet>`,
 })
-export class CoreComponent {}
+export class CoreComponent {
+  public constructor(private readonly serviceWorkerUpdate: SwUpdate) {
+    // Update the service worker on every construct
+    this.serviceWorkerUpdate.activateUpdate();
+  }
+}
