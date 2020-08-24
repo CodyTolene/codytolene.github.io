0b8a4ae7bcec19268269434d81b99d71562325c1
Cody Tolene <codyatolene@gmail.com>
2020-08-23 23:51:40 -0500

    Added responsive window width updates to the navigation bar

--- src/core/page-elements/navigation.component.ts
+++ src/core/page-elements/navigation.component.ts
@@ -1,7 +1,9 @@
 import { Component, HostListener, OnInit, Inject } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
-import { WINDOW } from 'src/services/browser';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { map } from 'rxjs/operators';
+import { Subject, BehaviorSubject } from 'rxjs';
+import { WINDOW } from 'src/services/browser';
 
 @Component({
   selector: 'app-navigation',
@@ -17,8 +19,20 @@ export class NavigationComponent implements OnInit {
   public distanceFromTopOfPage = 0;
   public readonly faBars = faBars;
 
+  private readonly windowWidthSubject = new BehaviorSubject<number>(
+    this.window.innerWidth,
+  );
+  public readonly isMobileDeviceWindowWidth = this.windowWidthSubject.pipe(
+    map((windowWidth) => windowWidth < 992),
+  );
+
   @HostListener('window:scroll', []) public onScroll(): void {
     this.distanceFromTopOfPage = this.getDistanceFromTopOfPage();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  public onResize(event): void {
+    this.windowWidthSubject.next(event.target.innerWidth);
   }
 
   public ngOnInit(): void {
