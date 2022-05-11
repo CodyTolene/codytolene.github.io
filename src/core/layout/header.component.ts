873d7c8081d79965a733b92959f36b931c64cead
Cody Tolene <codyatolene@gmail.com>
2022-05-10 23:37:55 -0500

    Start work on header

--- src/core/layout/header.component.ts
+++ src/core/layout/header.component.ts
@@ -2,7 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'ct-header',
-  styles: [],
-  template: ` <header>Header content.</header> `,
+  styles: [
+    `
+      :host,
+      header {
+        width: 100%;
+        height: 50px;
+        background: var(--theme_dark_gray);
+        z-index: 999;
+      }
+      header {
+        position: fixed;
+        top: 0;
+        width: 100%;
+
+        .row {
+          height: 50px;
+
+          .links {
+            text-align: end;
+          }
+        }
+      }
+    `,
+  ],
+  template: `
+    <header class="container-fluid">
+      <div class="row">
+        <div class="col-6 col-md-4">CODY TOLENE</div>
+        <div class="col-6 col-md-8 links">Links</div>
+      </div>
+    </header>
+  `,
 })
 export class HeaderComponent {}
