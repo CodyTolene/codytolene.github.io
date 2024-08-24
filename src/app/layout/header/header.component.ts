e762138f981937a0930cc7f044574f5b28bf7345
Cody Tolene <codyatolene@gmail.com>
2024-08-23 23:25:20 -0500

    Updating scroll

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,36 @@
+import { firstValueFrom, shareReplay } from 'rxjs';
+import { NavigationService } from 'src/app/services';
 import { scrollToElementById, scrollTop } from 'src/app/utilities';
 
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   selector: 'ct-header',
   standalone: true,
   styleUrl: './header.component.scss',
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
-  protected readonly scrollToElementById = scrollToElementById;
+  public constructor(private readonly navigationService: NavigationService) {}
+
   protected readonly scrollTop = scrollTop;
+
+  protected readonly currentUrl$ = this.navigationService.currentUrl$.pipe(
+    shareReplay(1),
+  );
+
+  private readonly isNavigating$ = this.navigationService.isNavigating$;
+
+  protected async scrollToElementById(id: string): Promise<void> {
+    const isNavigating = await firstValueFrom(this.isNavigating$);
+    if (isNavigating) {
+      await new Promise((resolve) => setTimeout(resolve, 250));
+      return this.scrollToElementById(id);
+    }
+    scrollToElementById(id);
+  }
 }
