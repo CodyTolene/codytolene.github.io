1ae40f58cd534ebf9457d87de1599ac306994cfc
Cody Tolene <codyatolene@gmail.com>
2024-08-24 16:57:50 -0500

    Home page, header, icon updates.

--- src/app/utilities/scroll.ts
+++ src/app/utilities/scroll.ts
@@ -1,20 +1,27 @@
 /** Smoothly scroll an element into view. */
-export function scrollToElement(element: HTMLElement): void {
-  element.scrollIntoView({
+export function scrollToElement(element: HTMLElement, offset = 60): void {
+  const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+  const offsetPosition = elementPosition - offset;
+  window.scrollTo({
+    top: offsetPosition,
     behavior: 'smooth',
-    block: 'start',
-    inline: 'nearest',
   });
 }
 
-/** Smoothly scroll to an element by ID. */
-export function scrollToElementById(id: string): void {
+/**
+ * Smoothly scroll to an element by ID.
+ *
+ * @param id The ID of the element to scroll to.
+ * @param offset The offset from the top of the element to scroll to. Default
+ * is 50 which is equal to the fixed header height.
+ */
+export function scrollToElementById(id: string, offset = 60): void {
   const element = document.getElementById(id);
   if (!element) {
     console.warn(`Failed to find and scroll to element with id "${id}".`);
     return;
   }
-  scrollToElement(element);
+  scrollToElement(element, offset);
 }
 
 /** Smoothly scroll to the top of the page. */
