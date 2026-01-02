27528fd6a135e52190726712ad02176502b3e82d
Cody Tolene <codyatolene@gmail.com>
2026-01-01 22:52:08 -0600

    Privacy policy

--- src/app/pages/privacy-policy/privacy-policy-page.ts
+++ src/app/pages/privacy-policy/privacy-policy-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'ct-privacy-policy-page',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './privacy-policy-page.html',
   styleUrl: './privacy-policy-page.scss',
 })
